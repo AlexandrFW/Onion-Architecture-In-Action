cf7039b593010a0e918dfab28af188040b7bbdd0
Alexandr Kutiev <Alexandr.Kutiev@yandex.ru>
2021-12-10 15:13:58 +0300

    Add one more instruction to the script to be able to run project on Linux Ubuntu 20.04-x64

--- Build_And_Publish_M01.ps1
+++ Build_And_Publish_M01.ps1
@@ -4,5 +4,8 @@ cd C:\C#_Projects\EPAM\M01\M01_ConsoleApp
 # Perform the Build
 dotnet build -c Release -o win10-x64
 
-# Publish the project
+# Publish the project to work on Windows 10 x64
 dotnet publish -c release -r win10-x64 --self-contained
+
+# Public the project to work on Ubuntu 20.04-x64
+dotnet publish -c Release -r ubuntu.20.04-x64 --self-contained
