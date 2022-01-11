6088e0d925bce6935baa864a09a5d66a8505502c
Alexandr Kutiev <Alexandr.Kutiev@yandex.ru>
2022-01-11 23:33:14 +0300

    Added Exception handling and logging project for M05 task

---
+++ M05_Exception_Handling_Logging_NLog/ExceptionHandlingAndLogApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using TypesConverterLibrary;
+
+namespace ExceptionHandlingAndLogApp
+{
+    public class Program
+    {
+        private static readonly NLog.Logger LoggerN = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("M05. Exception Handling. Logging. NLog");
+            Console.WriteLine("");
+
+            var host = Host.CreateDefaultBuilder(args).Build();       
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+            logger.LogInformation("Start HOST");
+            host.StartAsync();
+
+            logger.LogInformation("Init StringToIntConverter from TypeConverterLibrary.dll");
+            var toIntConverter = new StringToIntConverter(logger);
+
+            try
+            {
+                logger.LogInformation("Convert string to int");
+                toIntConverter.ConvertToInt("45467a");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+
+                logger.LogError("This exception has thrown from TypeConverterLibrary.dll\r\nMessage: {0}\r\nStackTrace: {1}", new object[] { ex.Message, ex.StackTrace });
+                LoggerN.Error(ex);
+            }
+
+            logger.LogInformation("Waiting for user reaction");
+            Console.ReadKey();
+
+            logger.LogInformation("Stop HOST asynchronously");
+            host.StopAsync().Wait();
+
+            logger.LogInformation("NLog shutting down");
+            NLog.LogManager.Shutdown(); 
+        }
+    }
+}
---
+++ M05_Exception_Handling_Logging_NLog/TypesConverterLibrary/StringToIntConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace TypesConverterLibrary
+{
+    public class StringToIntConverter
+    {
+        private readonly ILogger _logger;
+
+        public StringToIntConverter(ILogger logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("StringToIntConverter got the ILogger as a dependancy in a consructor");
+        }
+
+        public int ConvertToInt(string sInputValue)
+        {
+            if (string.IsNullOrWhiteSpace(sInputValue))
+                throw new ArgumentException($"Input parameter { nameof(sInputValue) } should not be empty string or whitespace");
+
+            try
+            {
+                _logger.LogInformation("Start converting string to int");
+                var nResult = IntParse(sInputValue);
+                _logger.LogInformation($"String converted to Int successfully. Result is { nResult }");
+
+                return nResult;
+                
+            }
+            catch (FormatException e)
+            {
+                _logger.LogError("Error has occured during string to int convertion\r\nMessage: {0}\r\nStackTrace: {1}", new object[] { e.Message, e.StackTrace });
+                throw;
+            }            
+        }
+
+        private int IntParse(string sValue)
+        {
+            int nResult = 0;
+
+            for (int i = 0; i < sValue.Length; i++)
+            {
+                if (!char.IsDigit(sValue[i]))
+                    throw new FormatException($"The given string contains not digit item at index { i }, value is \"{ sValue[i] }\" ");
+
+                nResult = 10 * nResult + (sValue[i] - 48);
+            }
+
+            return nResult;
+        }
+    }
+}
