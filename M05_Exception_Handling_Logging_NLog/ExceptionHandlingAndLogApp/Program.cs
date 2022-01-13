31bb01e08b5121728b323448d51912651d4f2504
Alexandr Kutiev <Alexandr.Kutiev@yandex.ru>
2022-01-13 12:42:42 +0300

    All comments has been fixed

--- M05_Exception_Handling_Logging_NLog/ExceptionHandlingAndLogApp/Program.cs
+++ M05_Exception_Handling_Logging_NLog/ExceptionHandlingAndLogApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using TypesConverterLibrary;
 
@@ -13,13 +14,20 @@ namespace ExceptionHandlingAndLogApp
         static void Main(string[] args)
         {
             Console.WriteLine("M05. Exception Handling. Logging. NLog");
-            Console.WriteLine("");
+            Console.WriteLine();
 
-            var host = Host.CreateDefaultBuilder(args).Build();       
-            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            Log.Logger = new LoggerConfiguration()
+                            .MinimumLevel.Debug()
+                            .WriteTo.Console(outputTemplate: "{Timestamp:dd.MM.yyyy HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}")
+                            .CreateLogger();
+
+            // Register Serilog while creating Microsoft.Extensions.Logging.LoggerFactory
+            using var loggerFactory = LoggerFactory.Create(builder => builder.AddSerilog(dispose: true));
+
+            // Create an instance of ILogger using the factory
+            var logger = loggerFactory.CreateLogger<Program>();
 
             logger.LogInformation("Start HOST");
-            host.StartAsync();
 
             logger.LogInformation("Init StringToIntConverter from TypeConverterLibrary.dll");
             var toIntConverter = new StringToIntConverter(logger);
@@ -33,17 +41,17 @@ namespace ExceptionHandlingAndLogApp
             {
                 Console.WriteLine();
 
-                logger.LogError("This exception has thrown from TypeConverterLibrary.dll\r\nMessage: {0}\r\nStackTrace: {1}", new object[] { ex.Message, ex.StackTrace });
+                logger.LogError("This exception has thrown from TypeConverterLibrary.dll\r\nMessage: {0}\r\nStackTrace: {1}", ex.Message, ex.StackTrace);
                 LoggerN.Error(ex);
             }
 
             logger.LogInformation("Waiting for user reaction");
-            Console.ReadKey();
-
-            logger.LogInformation("Stop HOST asynchronously");
-            host.StopAsync().Wait();
+            Console.ReadKey();            
 
             logger.LogInformation("NLog shutting down");
+
+            Log.CloseAndFlush();
+
             NLog.LogManager.Shutdown(); 
         }
     }
--- M05_Exception_Handling_Logging_NLog/TypesConverterLibrary/StringToIntConverter.cs
+++ M05_Exception_Handling_Logging_NLog/TypesConverterLibrary/StringToIntConverter.cs
@@ -10,26 +10,26 @@ namespace TypesConverterLibrary
         public StringToIntConverter(ILogger logger)
         {
             _logger = logger;
-            _logger.LogInformation("StringToIntConverter got the ILogger as a dependancy in a consructor");
+            _logger.LogInformation("ILogger conveyed to the StringToIntConverter constructer as a dependancy");
         }
 
         public int ConvertToInt(string sInputValue)
         {
             if (string.IsNullOrWhiteSpace(sInputValue))
-                throw new ArgumentException($"Input parameter { nameof(sInputValue) } should not be empty string or whitespace");
+                throw new ArgumentException("Input parameter {0} should not be empty string or whitespace", nameof(sInputValue));
 
             try
             {
                 _logger.LogInformation("Start converting string to int");
                 var nResult = IntParse(sInputValue);
-                _logger.LogInformation($"String converted to Int successfully. Result is { nResult }");
+                _logger.LogInformation("String converted to Int successfully. Result is {0}", nResult);
 
                 return nResult;
                 
             }
             catch (FormatException e)
             {
-                _logger.LogError("Error has occured during string to int convertion\r\nMessage: {0}\r\nStackTrace: {1}", new object[] { e.Message, e.StackTrace });
+                _logger.LogError("Error has occured during string to int convertion\r\nMessage: {0}\r\nStackTrace: {1}", e.Message, e.StackTrace);
                 throw;
             }            
         }
@@ -37,13 +37,14 @@ namespace TypesConverterLibrary
         private int IntParse(string sValue)
         {
             int nResult = 0;
+            int nOffsetFromChar_1_To_Integer_1_In_ASCII = 48;
 
             for (int i = 0; i < sValue.Length; i++)
             {
                 if (!char.IsDigit(sValue[i]))
-                    throw new FormatException($"The given string contains not digit item at index { i }, value is \"{ sValue[i] }\" ");
+                    throw new FormatException(string.Format("The given string contains not digit item at index {0}, value is {1} ", i, sValue[i]));
 
-                nResult = 10 * nResult + (sValue[i] - 48);
+                nResult = 10 * nResult + (sValue[i] - nOffsetFromChar_1_To_Integer_1_In_ASCII);
             }
 
             return nResult;
