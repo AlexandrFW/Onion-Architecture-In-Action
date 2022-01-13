4d90866a4850aa148e34e468007bf3f0bd3e6abf
Alexandr Kutiev <Alexandr.Kutiev@yandex.ru>
2022-01-13 12:51:31 +0300

    Added M06. Unit Testing

---
+++ M06_Unit_Testing/Readme.md
@@ -0,0 +1,8 @@
+# Homework 6
+
+## M06. Unit Testing
+## Description
+Cover with Unit Tests:
+
+M03 Tasks 2-5
+M05 Method, that allows to convert a string to integer
---
+++ M06_Unit_Testing/UnitTestingConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnitTestingConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Module 6. M06_Unit_Testing\r\n");            
+
+            Console.ReadKey();
+        }
+    }
+}
